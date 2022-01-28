a60a9a780b38c53d1660d48c154e9441c1f52a67
henrodorito <astarfall1288@gmail.com>
2022-01-28 16:54:35 -0500

    Calum: Another small update to intake code
    Intake not working yet

--- src/main/java/frc/robot/commands/ActivateIntakeCommand.java
+++ src/main/java/frc/robot/commands/ActivateIntakeCommand.java
@@ -24,21 +24,20 @@ public class ActivateIntakeCommand extends CommandBase {
     m_intake.toggleIntake();
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
+  public void end() {
     m_intake.toggleIntake();
   }
 
+  // Called every time the scheduler runs while the command is scheduled.
+  
+
+  
+  // Called once the command ends or is interrupted.
+ 
+  
+
   // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+
+  
+  
 }
