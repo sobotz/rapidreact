da2e30b361005a0713a2137078263f433400048f
henrodorito <92584298+lfermaintt@users.noreply.github.com>
2022-02-01 15:40:34 -0500

    Added condition for no target acquisition
    also bound vision command to button one

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import org.opencv.ml.LogisticRegression;
-
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -25,6 +23,9 @@ public class VisionSubsystem extends SubsystemBase {
   NetworkTableEntry tx;
   NetworkTableEntry ty;
   NetworkTableEntry tArea;
+  NetworkTableEntry tv;
+
+  boolean hasTarget;
 
   double xOffset;
   double yOffset;
@@ -36,6 +37,7 @@ public class VisionSubsystem extends SubsystemBase {
 
     this.table = NetworkTableInstance.getDefault().getTable("limelight");
 
+    this.tv = table.getEntry("tv");
     this.tx = table.getEntry("tx");
     this.ty = table.getEntry("ty");
     this.tArea = table.getEntry("ta");
@@ -44,11 +46,14 @@ public class VisionSubsystem extends SubsystemBase {
   }
 
   public void correctX () {
-    double speed = 2.0/(1.0 + Math.pow(Math.E, VisionConstants.LOGISTIC_GROWTH_RATE * this.xOffset)) - 1.0;
-    if (speed < VisionConstants.MIN_ADJUST_SPEED){
-      speed = VisionConstants.MIN_ADJUST_SPEED;
-    } else if (this.xOffset < VisionConstants.DEADBAND_RANGE) {
-      speed = 0.0;
+    double speed = 1;
+    if (this.hasTarget) {
+      speed = 2.0/(1.0 + Math.pow(Math.E, VisionConstants.LOGISTIC_GROWTH_RATE * this.xOffset)) - 1.0;
+      if (speed < VisionConstants.MIN_ADJUST_SPEED){
+        speed = VisionConstants.MIN_ADJUST_SPEED;
+      } else if (this.xOffset < VisionConstants.DEADBAND_RANGE) {
+        speed = 0.0;
+      }
     }
     this.actuationMotor.set(ControlMode.PercentOutput, speed);
   }
@@ -59,10 +64,13 @@ public class VisionSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    this.hasTarget = tv.getDouble(0.0) == 1.0;
+
     this.xOffset = tx.getDouble(0.0);
     this.yOffset = ty.getDouble(0.0);
     this.area = tArea.getDouble(0.0);
 
+    SmartDashboard.putBoolean("LimelightTarget", this.hasTarget);
     SmartDashboard.putNumber("LimelightX", this.xOffset);
     SmartDashboard.putNumber("LimelightY", this.yOffset);
     SmartDashboard.putNumber("LimelightArea", area);
