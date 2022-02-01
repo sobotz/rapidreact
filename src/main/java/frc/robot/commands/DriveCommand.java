cadb4023a6014014e822d5b3cfa961db10739355
henrodorito <92584298+lfermaintt@users.noreply.github.com>
2022-02-01 17:09:29 -0500

    Made the acceleration Constant changeable in SmartDashboard

--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -5,8 +5,10 @@
 package frc.robot.commands;
 
 import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
@@ -17,6 +19,8 @@ public class DriveCommand extends CommandBase {
 
   private Joystick joystick;
 
+  private double acceleration_constant;
+
   /**
    * Creates a new ExampleCommand.
    *
@@ -25,6 +29,8 @@ public class DriveCommand extends CommandBase {
   public DriveCommand(DriveSubsystem drivetrain, Joystick joystick) {
     m_drivetrain = drivetrain;
     this.joystick = joystick;
+    this.acceleration_constant = DriveConstants.ACCELERATION_CONSTANT;
+    SmartDashboard.putNumber("Acceleration Constant: ", this.acceleration_constant);
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(m_drivetrain);
   }
@@ -36,10 +42,11 @@ public class DriveCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    this.acceleration_constant = SmartDashboard.getNumber("Acceleration Constant: ", DriveConstants.ACCELERATION_CONSTANT);
     double speed = this.joystick.getY();
     double rotation = this.joystick.getX();
-    double squaredSpeed = Math.signum(speed) * Math.pow(speed, Constants.DriveConstants.ACCELERATION_CONSTANT);
-    double squaredRotation = Math.signum(rotation) * Math.pow(rotation, Constants.DriveConstants.ACCELERATION_CONSTANT);
+    double squaredSpeed = Math.signum(speed) * Math.pow(speed, this.acceleration_constant);
+    double squaredRotation = Math.signum(rotation) * Math.pow(rotation, this.acceleration_constant);
     this.m_drivetrain.drive(squaredSpeed, -squaredRotation);
   }
 
