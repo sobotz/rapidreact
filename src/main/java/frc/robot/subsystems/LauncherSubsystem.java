1087ee9aa5b2ece496ebd4caeb371314bf8a5e47
seanma711 <92584298+lfermaintt@users.noreply.github.com>
2022-02-14 16:23:06 -0500

    update to launcher subsystem

--- src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -4,14 +4,46 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.LauncherConstants;
 
 public class LauncherSubsystem extends SubsystemBase {
   /** Creates a new LauncherSubsystem. */
-  public LauncherSubsystem() {}
+  public WPI_TalonFX launcherMotor;
+  public WPI_TalonFX launcherMotor2;
+  WPI_TalonSRX feedMotor;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public LauncherSubsystem() {
+    launcherMotor = new WPI_TalonFX(LauncherConstants.LAUNCHER_MOTOR_1);
+    launcherMotor2 = new WPI_TalonFX(LauncherConstants.LAUNCHER_MOTOR_2);
+    feedMotor = new WPI_TalonSRX(Constants.ROLLER_MOTOR);
+
+    launcherMotor.configFactoryDefault();
+    launcherMotor2.configFactoryDefault();
+    feedMotor.configFactoryDefault();
+  }
+
+  public void startRollers() {
+    feedMotor.set(ControlMode.PercentOutput, 1);
+  }
+
+  public void stopRollers() {
+    feedMotor.set(ControlMode.PercentOutput, 0);
+  }
+
+  public void startLauncher() {
+    launcherMotor.set(ControlMode.PercentOutput, 1);
+    launcherMotor.setInverted(true);
+    launcherMotor2.follow(launcherMotor);
+  }
+
+  public void stopLauncher() {
+    launcherMotor.set(ControlMode.Velocity, 0);
+    launcherMotor2.set(ControlMode.Velocity, 0);
   }
 }
