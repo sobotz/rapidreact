9ec54bbd65ee55a90e1f1b137524c5f62d858b2b
MavG2 <92941727+CalT128@users.noreply.github.com>
2022-02-15 16:32:41 -0500

    ActivateLauncherCOmmand -calum

--- src/main/java/frc/robot/commands/ActivateLauncherCommand.java
+++ src/main/java/frc/robot/commands/ActivateLauncherCommand.java
@@ -5,28 +5,57 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LauncherSubsystem;
+import frc.robot.subsystems.SerializerSubsystem;
 
 public class ActivateLauncherCommand extends CommandBase {
-  /** Creates a new ActivateLauncherCommand. */
-  public ActivateLauncherCommand() {
+  private SerializerSubsystem serializer;
+  private LauncherSubsystem launcher;
+  private int nFramesRun;
+
+  /**
+   * Creates a new LaunchAllCommand.
+   */
+  public ActivateLauncherCommand(SerializerSubsystem serializer1, LauncherSubsystem launcher1) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.serializer = serializer1;
+    this.launcher = launcher1;
+    addRequirements(serializer, launcher);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    this.nFramesRun = 0;
+    this.launcher.startLauncher();
+    this.serializer.acceptingBalls = false;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //launcher.get_velocity maybe
+    if (this.nFramesRun > 50) {
+      this.launcher.startRollers();
+      this.serializer.runBelts();
+    }
+
+    this.nFramesRun++;
+ 
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    this.launcher.stopRollers();
+    this.launcher.stopLauncher();
+    this.serializer.stopBelts();
+    this.serializer.acceptingBalls = true;
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+     return this.nFramesRun > 500;
   }
 }
